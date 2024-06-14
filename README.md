3a6908feb705f43bd8d259e0d9cef77a09cadc4c
Giovanna <maria.giovanna@escolar.ifrn.edu.br>
2024-06-14 14:32:23 -0300

    feat: added react-hook-forms functions

--- README.md
+++ README.md
@@ -1,70 +1,19 @@
-# Getting Started with Create React App
+# React Forms Transformation
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Introduction
 
-## Available Scripts
+This project demonstrates the transformation of a poorly designed form (`BadForm`) into a well-structured and efficient form (`GoodForm`) using the capabilities of the `react-hook-form` library. `react-hook-form` simplifies form handling in React by providing a set of hooks to manage form state and validation.
 
-In the project directory, you can run:
+## Features
 
-### `npm start`
+- Comparison between `BadForm` and `GoodForm`.
+- Utilizes `react-hook-form` for form state management and validation.
+- Showcases best practices in form handling with React.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Installation
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+To get started with this project, follow these steps:
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/gigigabytes/react_forms.git
--- src/components/GoodForm.jsx
+++ src/components/GoodForm.jsx
@@ -1,137 +1,150 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { useForm } from "react-hook-form";
-import { isEmail } from "validator";
+import validator from "validator";
 
 const GoodForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+    } = useForm();
 
-  const watchPassword = watch("password");
+    console.log(errors);
 
-  const onSubmit = (data) => {
-    alert(JSON.stringify(data));
-  };
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    };
 
-  console.log("RENDER");
+    const watchPassword = watch("password");
 
-  return (
-    <div className="app-container">
-      <div className="form-group">
-        <label>Name</label>
-        <input
-          className={errors?.name && "input-error"}
-          type="text"
-          placeholder="Your name"
-          {...register("name", { required: true })}
-        />
-        {errors?.name?.type === "required" && (
-          <p className="error-message">Name is required.</p>
-        )}
-      </div>
+    return (
+        <div className="app-container">
+            <div className="form-group">
+                <label>Nome</label>
+                <input
+                    className={errors?.name && "input-error"}
+                    type="text"
+                    placeholder="Seu nome"
+                    {...register("name", { required: true })} //chama um parametro que é o nome do input
+                />
+                {errors?.name?.type === "required" && (
+                    <p className="error-message">Name is required</p>
+                )}
+            </div>
 
-      <div className="form-group">
-        <label>E-mail</label>
-        <input
-          className={errors?.email && "input-error"}
-          type="email"
-          placeholder="Your e-mail"
-          {...register("email", {
-            required: true,
-            validate: (value) => isEmail(value),
-          })}
-        />
-        {errors?.email?.type === "required" && (
-          <p className="error-message">Email is required.</p>
-        )}
+            <div className="form-group">
+                <label>E-mail</label>
+                <input
+                    className={errors?.email && "input-error"}
+                    type="email"
+                    placeholder="Seu e-mail"
+                    {...register("email", {
+                        required: true,
+                        validate: (value) => validator.isEmail(value),
+                    })}
+                />
+                {errors?.email?.type === "required" && (
+                    <p className="error-message">E-mail required</p>
+                )}
+                {errors?.email?.type === "validate" && (
+                    <p className="error-message">E-mail is invalid</p>
+                )}
+            </div>
 
-        {errors?.email?.type === "validate" && (
-          <p className="error-message">Email is invalid.</p>
-        )}
-      </div>
+            <div className="form-group">
+                <label>Senha</label>
+                <input
+                    className={errors?.password && "input-error"}
+                    type="password"
+                    placeholder="Senha"
+                    {...register("password", { required: true, minLength: 7 })}
+                />
+                {errors?.password?.type === "minLength" && (
+                    <p className="error-message">
+                        Password must have at least 7 characters.
+                    </p>
+                )}
 
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          className={errors?.password && "input-error"}
-          type="password"
-          placeholder="Password"
-          {...register("password", { required: true, minLength: 7 })}
-        />
+                {errors?.password?.type === "required" && (
+                    <p className="error-message">Password is required</p>
+                )}
+            </div>
 
-        {errors?.password?.type === "required" && (
-          <p className="error-message">Password is required.</p>
-        )}
+            <div className="form-group">
+                <label>Confirmação de senha</label>
+                <input
+                    className={errors?.passwordConfirmation && "input-error"}
+                    type="password"
+                    placeholder="Digite sua senha novamente"
+                    {...register("passwordConfirmation", {
+                        required: true,
+                        minLength: 7,
+                        validate: (value) => value === watchPassword,
+                    })}
+                />
 
-        {errors?.password?.type === "minLength" && (
-          <p className="error-message">
-            Password needs to have at least 7 characters.
-          </p>
-        )}
-      </div>
+                {errors?.passwordConfirmation?.type === "required" && (
+                    <p className="error-message">
+                        Password confirmation is required
+                    </p>
+                )}
+                 {errors?.passwordConfirmation?.type === "validate" && (
+                    <p className="error-message">
+                        Password does not match
+                    </p>
+                )}
+                {errors?.passwordConfirmation?.type === "minLength" && (
+                    <p className="error-message">
+                        Password confirmation must have at least 7 characters.
+                    </p>
+                )}
+            </div>
 
-      <div className="form-group">
-        <label>Password confirmation</label>
-        <input
-          className={errors?.passwordConfirmation && "input-error"}
-          type="password"
-          placeholder="Repeat your password"
-          {...register("passwordConfirmation", {
-            required: true,
-            validate: (value) => value === watchPassword,
-          })}
-        />
-        {errors?.passwordConfirmation?.type === "required" && (
-          <p className="error-message">Password confirmation is required.</p>
-        )}
+            <div className="form-group">
+                <label>Profissão</label>
+                <select
+                    {...register("profession", {
+                        validate: (value) => {
+                            return value !== "0";
+                        },
+                    })}
+                    className={errors?.profession && "input-error"}
+                >
+                    <option value="0">Selecione sua profissão...</option>
+                    <option value="developer">Desenvolvedor</option>
+                    <option value="other">Outra</option>
+                </select>
 
-        {errors?.passwordConfirmation?.type === "validate" && (
-          <p className="error-message">Passwords does not match.</p>
-        )}
-      </div>
-      <div className="form-group">
-        <label>Profession</label>
-        <select
-          className={errors?.profession && "input-error"}
-          defaultValue="0"
-          {...register("profession", { validate: (value) => value !== "0" })}
-        >
-          <option value="0">Select your profession...</option>
-          <option value="developer">Developer</option>
-          <option value="other">Other</option>
-        </select>
+                {errors?.profession?.type === "validate" && (
+                    <p className="error-message">Profession is required</p>
+                )}
+            </div>
 
-        {errors?.profession?.type === "validate" && (
-          <p className="error-message">Profession is required.</p>
-        )}
-      </div>
+            <div className="form-group">
+                <div className="checkbox-group">
+                    <input
+                        type="checkbox"
+                        name="privacy-policy"
+                        {...register("privacyTerms", { required: true })}
+                    />
+                    <label>I agree with the privacy terms.</label>
+                </div>
 
-      <div className="form-group">
-        <div className="checkbox-group">
-          <input
-            type="checkbox"
-            name="privacy-policy"
-            {...register("privacyTerms", {
-              validate: (value) => value === true,
-            })}
-          />
-          <label>I agree with the privacy terms.</label>
+                {errors?.privacyTerms?.type === "required" && (
+                    <p className="error-message">
+                        You must agree with the privacy terms
+                    </p>
+                )}
+            </div>
+            {/* //chamar função que vai ser retornada */}
+            <div className="form-group">
+                <button onClick={() => handleSubmit(onSubmit)()}>
+                    Criar conta
+                </button>
+            </div>
         </div>
-
-        {errors?.privacyTerms?.type === "validate" && (
-          <p className="error-message">
-            You must agree with the privacy terms.
-          </p>
-        )}
-      </div>
-
-      <div className="form-group">
-        <button onClick={() => handleSubmit(onSubmit)()}>Criar conta</button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default GoodForm;
